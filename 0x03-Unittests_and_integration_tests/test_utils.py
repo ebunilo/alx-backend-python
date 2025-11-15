7e745123009a2b69f7f84054159e643531e13fa5
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-15 19:27:11 +0100

    update parameterized values

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,8 +6,12 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    '''TestAccessNestedMap class to test access_nested_map function
+    from utils module.
+    '''
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
