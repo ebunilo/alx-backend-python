8e7b6005c72af94f013a40cf3fb136ab33ff3395
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-15 20:57:42 +0100

    Unit test for memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -44,8 +44,29 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
-    
-        
+
+
+class TestMemoize(unittest.TestCase):
+    '''Tests for memoize decorator.'''
+    def test_memoize(self):
+        from utils import memoize
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            first = obj.a_property
+            second = obj.a_property
+            self.assertEqual(first, 42)
+            self.assertEqual(second, 42)
+            mock_method.assert_called_once()
+
 
 if __name__ == "__main__":
     unittest.main()
