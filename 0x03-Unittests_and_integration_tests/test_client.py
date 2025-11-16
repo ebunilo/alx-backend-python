92dd0041408f71f9c8d503fd65a9f3e6c89979be
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-16 19:51:08 +0100

    Unit test for GithubOrgClient

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, mock_get_json, org_name):
+        expected = {"name": org_name, "repos_url": f"https://api.github.com/orgs/{org_name}/repos"}
+        mock_get_json.return_value = expected
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, expected)
+        mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+
+
+if __name__ == '__main__':
+    unittest.main()
