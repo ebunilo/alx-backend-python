bdd8f656ee1e2fd96a15ca24a99bf39c5039a579
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-17 16:39:53 +0100

    roll back JWT token authentication

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -15,15 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # conversations & messages endpoints
     path('api-auth/', include('rest_framework.urls')),  # login/logout for browsable API
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # JWT login
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # JWT refresh
 ]
