ddfd10f1f37f981b4a4d3937a0399294071af0ce
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-17 16:15:38 +0100

    implement jwt token authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -61,6 +61,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # Add JWT
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
