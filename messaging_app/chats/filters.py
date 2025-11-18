577571f7957cc4a654915f54e7f0c38ea4b30224
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-18 12:41:26 +0100

    add results of Postman testing

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -2,17 +2,19 @@ import django_filters
 from .models import Message
 
 class MessageFilter(django_filters.FilterSet):
-    created_after = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='gte')
-    created_before = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
-    user = django_filters.NumberFilter(method='filter_user')
+    # Keep query param names created_after/created_before but map to sent_at
+    created_after = django_filters.DateTimeFilter(field_name='sent_at', lookup_expr='gte')
+    created_before = django_filters.DateTimeFilter(field_name='sent_at', lookup_expr='lte')
+    user = django_filters.UUIDFilter(method='filter_user')  # User has UUID primary key
 
     def filter_user(self, queryset, name, value):
-        return queryset.filter(sender_id=value) | queryset.filter(recipient_id=value)
+        # Messages sent by user OR in conversations the user participates in
+        return queryset.filter(sender_id=value) | queryset.filter(conversation__participants__id=value)
 
     class Meta:
         model = Message
         fields = [
             'conversation',
             'sender',
-            'created_at',
+            'sent_at',  # use existing field
         ]
