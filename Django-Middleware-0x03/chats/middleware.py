42aa2ef201c042feddc4747472211ce47eb64cfc
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-18 21:12:15 +0100

    capture logs

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.conf import settings
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.log_path = settings.BASE_DIR / 'requests.log'
+
+    def __call__(self, request):
+        user = getattr(request, 'user', None)
+        user_label = user.username if getattr(user, 'is_authenticated', False) else 'Anonymous'
+        try:
+            with open(self.log_path, 'a', encoding='utf-8') as fh:
+                fh.write(f"{datetime.now()} - User: {user_label} - Path: {request.path}\n")
+        except Exception:
+            pass
+        return self.get_response(request)
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',  # added
 ]
 
 # Django REST Framework configuration
