6c9c292de38c0cb6fa97de2df211f79e09d2b611
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2025-11-24 03:24:40 +0100

    add files

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+
+User = get_user_model()
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['receiver', 'created_at']),
+        ]
+
+    def __str__(self):
+        return f'Msg {self.id} from {self.sender} to {self.receiver}'
+
+
+class Notification(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['user', 'read', 'created_at']),
+        ]
+
+    def __str__(self):
+        return f'Notification {self.id} to {self.user} (read={self.read})'
